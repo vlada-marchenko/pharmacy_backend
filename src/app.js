e75e1490df34e886e3c7acb021f5206cc4827dca
Vlada Marchenko <160398216+vlada-marchenko@users.noreply.github.com>
2026-02-26 03:02:17 +0100

    Set request body size limit to 10mb
    Increase the JSON and URL-encoded request body size limit to 10mb.

--- src/app.js
+++ src/app.js
@@ -10,7 +10,8 @@ import statsRouter from './routes/stats.routes.js'
 export const app = express()
 
 app.use(cors())
-app.use(express.json())
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }))
 
 app.get('/', (req, res) => {
   res.send('API is running')
